8b48d63e26a4563ecbcdf6ace05ad0a6046ddf65
TamaraMikitich <mikitich.tamara@gmail.com>
2025-09-22 21:15:49 +0300

    Revert "teat: test"
    This reverts commit 2e6b9e4fe09a89021b6efaab507d99595c6c7694.

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -198,17 +198,10 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(str) {
-  if (str.length === 0) {
-    return 0;
-  }
-  let number = 0;
-  for (let i = 0; i < str.length; ) {
-    const charCode = str.charCodeAt(i);
-    number += charCode;
-  }
-  return number;
+function sumOfCodes(/* str */) {
+  throw new Error('Not implemented');
 }
+
 /**
  * Checks if a string starts with a specific substring.
  *
@@ -220,8 +213,8 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(str, substr) {
-  return str.startsWith(substr);
+function startsWith(/* str, substr */) {
+  throw new Error('Not implemented');
 }
 
 /**
